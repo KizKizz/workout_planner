c93c787b26b07dae5d8e2bf6bbc555fc5066d330
KizKizz <101075148+KizKizz@users.noreply.github.com>
2023-02-18 20:34:26 -0800

    selection page changes

--- lib/main.dart
+++ lib/main.dart
@@ -154,6 +154,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
+                  constraints: const BoxConstraints(minHeight: 250, minWidth: 250),
                   decoration: BoxDecoration(borderRadius: const BorderRadius.all(Radius.circular(10.0)), color: Theme.of(context).highlightColor),
                   width: constraints.maxWidth * 0.3,
                   height: constraints.maxWidth * 0.3,
